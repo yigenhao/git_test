47ca3ec04a27670c127fd1ed89d7666d731bd30c
snake <snake@gu.com>
2021-09-26 08:52:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git_test/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git_test;
+
+/**
+ * @author snake
+ * @create 2021-09-26 8:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test 第一次commit");
+    }
+}
